2355add5b58c9ac20aa5d4cb8d0a9c169729c54b
nizhen <nizhen>
2022-04-09 15:26:14 +0900

    update

--- script/figure8/bert_coarse_sparta/run.sh
+++ script/figure8/bert_coarse_sparta/run.sh
@@ -1,5 +1,18 @@
+python bert_codegen.py
 rm ~/.cache/nnfusion/kernel_cache.db
 python prepare_kernel_cfg.py --in_dir ../../checkpoints/bert/artifact_bert_coarse_onnx_with_tesa --out_dir ./nnfusion_cfg_bert_coarse
 pushd ./nnfusion_cfg_bert_coarse
 nnfusion model.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config
 #nnfusion model.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fblockfusion_level=0 -fcodegen_debug=true -fspargen_cfg config
+pushd nnfusion_rt/cuda_codegen
+mkdir build
+pushd build 
+cmake ..
+make 
+ln -s ../Constant
+./main_test
+popd
+popd
+
+popd
+
