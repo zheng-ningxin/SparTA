579e5d4075e06d42b9057bca173afa7a681787fa
nizhen <nizhen>
2022-04-09 13:28:37 +0900

    update

--- script/init_env.sh
+++ script/init_env.sh
@@ -2,5 +2,7 @@ source ~/anaconda/etc/profile.d/conda.sh
 conda activate artifact
 
 HTTP_PORT=8881 BACKEND=c-cuda nohup antares rest-server &
-mkdir ~/.cache/antares/codehub/
+# wait the antares to be ready
+sleep 5s
+mkdir -p ~/.cache/antares/codehub/
 cp figure8/codehub/* ~/.cache/antares/codehub/
