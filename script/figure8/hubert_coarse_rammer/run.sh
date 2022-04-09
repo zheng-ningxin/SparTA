2cdeae38943e5fee762720d27cd5e947ccaba5c3
nizhen <nizhen>
2022-04-09 12:58:20 +0900

    update

--- script/figure8/hubert_coarse_rammer/run.sh
+++ script/figure8/hubert_coarse_rammer/run.sh
@@ -1,3 +1,11 @@
 cp ../../checkpoints/hubert/hubert_ori_no_tesa.onnx .
 nnfusion hubert_ori_no_tesa.onnx -f onnx
-
+pushd nnfusion_rt/cuda_codegen
+mkdir build
+pushd build
+cmake ..
+make
+ln -s ../Constant
+./main_test
+popd
+popd
