712dad52b3444dd13fa42bceb3483d2fc249796f
nizhen <nizhen>
2022-04-09 15:58:39 +0900

    update

--- script/figure8/hubert_coarse_sparta/run.sh
+++ script/figure8/hubert_coarse_sparta/run.sh
@@ -3,5 +3,14 @@ mkdir nnfusion_cfg
 cp artifact_hubert_coarse_onnx_with_tesa/model_tesa.onnx nnfusion_cfg
 pushd nnfusion_cfg
 nnfusion model_tesa.onnx -f onnx
+pushd nnfusion_rt/cuda_codegen
+mkdir build
+pushd build
+cmake ..
+make
+ln -s ../Constant
+./main_test
+popd
+popd
 popd
 
