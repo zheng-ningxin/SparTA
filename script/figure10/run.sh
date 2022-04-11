3434f7ec8829d1407861f6160fef1659da5e914c
nizhen <nizhen>
2022-04-11 17:21:42 +0900

    update

--- script/figure10/run.sh
+++ script/figure10/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+source ~/anaconda/etc/profile.d/conda.sh
+conda activate artifact
+pip install transformers==3.5.0
+
 cur_dir=`pwd`
 mkdir -p ${cur_dir}/log
 arr=("jit" "tvm" "tvm-s" "trt" "rammer" "rammer-s" "sparta")
- 
 for framework in ${arr[@]}
 do
     pushd $framework
