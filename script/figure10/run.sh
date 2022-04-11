46138c9ff6834c99203f46a01d5d2cc1c215c820
nizhen <nizhen>
2022-04-11 17:04:21 +0900

    update

--- script/figure10/run.sh
+++ script/figure10/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+cur_dir=`pwd`
+mkdir -p ${cur_dir}/log
 arr=("jit" "tvm" "tvm-s" "trt" "rammer" "rammer-s" "sparta")
  
 for framework in ${arr[@]}
 do
     pushd $framework
-    bash run.sh
+    bash run.sh > ${cur_dir}/log/${framework}.log
     popd
 done
+
+python draw.py
