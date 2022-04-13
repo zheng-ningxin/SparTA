378e420dcf4ccdbfa5d4dd0d4992140035dfe018
nizhen <nizhen>
2022-04-13 21:04:48 +0900

    update

--- script/README.md
+++ script/README.md
@@ -13,6 +13,7 @@ git clone https://github.com/zheng-ningxin/SparTA.git
 conda activate artifact
 cd SparTA && python setup.py develop
 cd script && bash init_env.sh
+bash run_all_2080ti.sh
 ```
 
 # for all
--- script/run_all_2080ti.sh
+++ script/run_all_2080ti.sh
@@ -3,7 +3,6 @@
 echo "Get the propagated mask/cks"
 bash init_checkpoints.sh
 
-
 echo "reproducing figure8"
 pushd figure8
 bash run.sh
@@ -24,13 +23,28 @@ pushd figure13
 bash run.sh
 popd
 
+echo "reproducing figure14"
+pushd figure14
+bash run.sh
+popd
+
+
 echo "reproducing figure15"
 pushd figure15
 bash run.sh
 popd
 
+echo "reproducing figure17"
+pushd figure17
+bash run.sh
+popd
 
 echo "reproducing figure19"
 pushd figure19
+bash run.sh
+popd
+
+echo "reproduing acc in table3"
+push table3
 bash run.sh
 popd
