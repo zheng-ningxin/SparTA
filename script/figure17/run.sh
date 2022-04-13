89f23cbe954d24665ca8f56d4778a04706cfa399
nizhen <nizhen>
2022-04-13 12:08:50 +0900

    update

--- script/figure17/run.sh
+++ script/figure17/run.sh
@@ -1,7 +1,7 @@
 source ~/anaconda/etc/profile.d/conda.sh
 conda activate artifact
 nvcc -gencode arch=compute_75,code=sm_75 -lcublas -o cubls cublas.cu
-nvcc -gencode arch=compute_75,code=sm_75-lcupsarse -o cusparse cusparse.cu
+nvcc -gencode arch=compute_75,code=sm_75 -lcusparse -o cusparse cusparse.cu
 nvcc -forward-unknown-to-host-compiler -I/usr/local/cuda/include -I/root/sputnik  -L/usr/local/cuda/lib64  -L/usr/local/lib -lcudart -lspmm  --generate-code=arch=compute_75,code=sm_75 -std=c++14  sputnik.cu -o sputnik
 mkdir log
 for sparsity in 0.5 0.7 0.8 0.9 0.95 0.99
