2023ebe121aa7e9caa724f586284fc1ba46c6489
nizhen <nizhen>
2022-04-16 03:27:22 +0900

    change position for run.sh

--- script/figure11/bert_coarse_int8/propagation/run.sh
+++ script/figure11/bert_coarse_int8/propagation/run.sh
@@ -1,6 +1,6 @@
-python bert_codegen_int8.py
 rm ~/.cache/nnfusion/kernel_cache.db
 cp -r ../../../checkpoints/bert/artifact_bert_coarse_onnx_with_tesa .
+python bert_codegen_int8.py
 python prepare_kernel_cfg.py --in_dir artifact_bert_coarse_onnx_with_tesa --out_dir ./nnfusion_cfg
 pushd ./nnfusion_cfg
 nnfusion model_tesa.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config -frun_step 500
