f5d4a05746d81ac023354011923cd13b8cae8ec0
nizhen <nizhen>
2022-08-10 15:41:05 +0900

    update longformer softmax

--- test/longformer_softmax_perf.cu
+++ test/longformer_softmax_perf.cu
@@ -298,72 +298,188 @@ __global__ void longformer_mixed_softmax_kernel_v2(float * A,
     each row of blocks is dealt with a thread group
     each block is 32x32
     */
+    const int ROW_TILE = 4;
+    assert(row_tile == ROW_TILE);
+    const int ROW_MAX_SIZE = 1024;
+    const int WARP_SIZE = 32;
+    __shared__ float shared_A[ROW_TILE*ROW_MAX_SIZE];
+    __shared__ float shared_M[ROW_TILE*ROW_MAX_SIZE];
+    float * As = shared_A;
+    float * Ms = shared_M;
     A += M * N * blockIdx.y;
-    extra_buffer += M * global_attention_size * blockIdx.y;
+    // extra_buffer += M * global_attention_size * blockIdx.y;
     uint blk_row_idx = blockIdx.x / (block_h/row_tile) ;
     int block_inter_row = (blockIdx.x % (block_h/row_tile)) * row_tile;
-    uint bm = threadIdx.x / block_w;
-    uint bn = threadIdx.x % block_w;
+    uint bm = threadIdx.x / WARP_SIZE;
+    uint bn = threadIdx.x % WARP_SIZE;
     assert(block_w % 32==0);
     float regC = 0.0f;
     float regSum = 0.0f;
     float regMax = -100000.0;
     int block_seq_start = row[blk_row_idx];
     int block_seq_end = row[blk_row_idx+1];
-    uint A_index, col_idx, mask_index;
+    // printf("block_seq_start:%d block_seq_end:%d row_size: %d ROW_MAX_SIZE:%d\n", block_seq_start, block_seq_end, (block_seq_end-block_seq_start) * block_w + global_attention_size, ROW_MAX_SIZE);
+    assert((block_seq_end-block_seq_start) * block_w + global_attention_size < ROW_MAX_SIZE);
+    uint A_index, col_idx, mask_index, shared_index;
+    As += ROW_MAX_SIZE * int(threadIdx.x/WARP_SIZE);
+    Ms += ROW_MAX_SIZE * int(threadIdx.x/WARP_SIZE);
+    // load from the global memory to the shared memory
+    #pragma unroll
     for(int i=bn; i<global_attention_size; i+=32){
         A_index = (blockIdx.x * row_tile + bm) * N + global_attention[i];
-        regMax = max(regMax, A[A_index]);
+        As[i] = A[A_index];
+        Ms[i] = 1.0;
+        A[A_index] = -10000.0; // 
     }
+    #pragma unroll
     for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
+        // FIXME: there need one more for loop to handle the WARP_SIZE -> BLOCK_W
         mask_index = block_h * block_w * block_seq + (block_inter_row + bm) * block_w + bn;
         A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
-        regMax = max(regMax, A[A_index] * val_mask[mask_index]);
-
+        As[global_attention_size + block_w * (block_seq - block_seq_start) + bn] = A[A_index];
+        Ms[global_attention_size + block_w * (block_seq - block_seq_start) + bn] = val_mask[mask_index];
     }
+    // calculate the softmax in the shared memory
+    // calculate the regMax
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        regMax = max(regMax, As[i]*Ms[i]);
+    }
+    #pragma unroll
     for (int offset = 16; offset > 0; offset /= 2) {
         regMax = max(regMax, __shfl_down_sync(FULL_MASK, regMax, offset));
     }
-    regMax = __shfl_sync(FULL_MASK, regMax, 0);
-
-    for(int i=bn; i<global_attention_size; i+=32){
-        A_index = (blockIdx.x * row_tile + bm) * N + global_attention[i];
-        regC = expf(A[A_index]-regMax);
-        regSum += regC;
-        A[A_index] = -10000.0;
-        extra_buffer[(blockIdx.x * row_tile + bm)*global_attention_size+ i] = regC; 
+    regMax = __shfl_sync(FULL_MASK, regMax, 0);    
+    // calculate the regSum
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        regSum += Ms[i] * expf(As[i]-regMax);
     }
-    for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
-        mask_index = block_h * block_w * block_seq + (block_inter_row + bm) * block_w + bn;
-        A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
-        if (val_mask[mask_index] != 0) {
-            regC = expf(A[A_index]-regMax);
-            regSum += regC;
-        }
-    }
+    #pragma unroll
     for (int offset = 16; offset > 0; offset /= 2) {
         regSum += __shfl_down_sync(FULL_MASK, regSum, offset);
     }
     regSum = __shfl_sync(FULL_MASK, regSum, 0);
-    // if(threadIdx.x%32==1)
-    //     printf("Row %d Regsum %f  \n", block_inter_row + bm + blk_row_idx * block_h, regSum);
-    for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
-        mask_index = block_h * block_w * block_seq + (block_inter_row + bm) * block_w + bn;
-        A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
-        regC = 0.0f;
-        if (val_mask[mask_index] > 0) {
-            A[A_index] = expf(A[A_index]-regMax)/regSum;
-        }
-        else{
-            A[A_index] = 0;
-        }
+    // calculate the corresponding value for each element
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        As[i] = expf(As[i] - regMax) / regSum;
+    }
 
+    // Write the results back to the global memory
+    #pragma unroll
+    for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
+        /// TODO FIXME: need one more loop from the BLOCK_W to the WARP_SIZE
+        A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
+        shared_index = global_attention_size+(block_seq-block_seq_start) * block_w + bn;
+        A[A_index] = As[shared_index] * Ms[shared_index]; 
     }
     for(int i=bn; i<global_attention_size; i+=32){
         A_index = (blockIdx.x * row_tile + bm) * N + global_attention[i];
-        A[A_index] = extra_buffer[(blockIdx.x * row_tile + bm)*global_attention_size+ i]/regSum; 
+        A[A_index] = As[i]; 
     }
 
+}
+
+__global__ void longformer_mixed_softmax_kernel_v3(float * A,
+                                     int * row,
+                                     int *col,
+                                     float* val_mask,
+                                     int * global_attention,
+                                     float* extra_buffer,
+                                     int block_h,
+                                     int block_w,
+                                     int block_nnz,
+                                     int row_tile,
+                                     int M,
+                                     int N,
+                                     int global_attention_size)
+{
+    /*
+    description:
+    each row of blocks is dealt with a thread group
+    each block is 32x32
+    */
+    const int ROW_TILE = 4;
+    assert(row_tile == ROW_TILE);
+    const int ROW_MAX_SIZE = 1024;
+    const int WARP_SIZE = 32;
+    __shared__ float shared_A[ROW_TILE*ROW_MAX_SIZE];
+    __shared__ float shared_M[ROW_TILE*ROW_MAX_SIZE];
+    float * As = shared_A;
+    float * Ms = shared_M;
+    A += M * N * blockIdx.y;
+    // extra_buffer += M * global_attention_size * blockIdx.y;
+    uint blk_row_idx = blockIdx.x / (block_h/row_tile) ;
+    int block_inter_row = (blockIdx.x % (block_h/row_tile)) * row_tile;
+    uint bm = threadIdx.x / WARP_SIZE;
+    uint bn = threadIdx.x % WARP_SIZE;
+    assert(block_w % 32==0);
+    float regC = 0.0f;
+    float regSum = 0.0f;
+    float regMax = -100000.0;
+    int block_seq_start = row[blk_row_idx];
+    int block_seq_end = row[blk_row_idx+1];
+    // printf("block_seq_start:%d block_seq_end:%d row_size: %d ROW_MAX_SIZE:%d\n", block_seq_start, block_seq_end, (block_seq_end-block_seq_start) * block_w + global_attention_size, ROW_MAX_SIZE);
+    assert((block_seq_end-block_seq_start) * block_w + global_attention_size < ROW_MAX_SIZE);
+    uint A_index, col_idx, mask_index, shared_index;
+    As += ROW_MAX_SIZE * int(threadIdx.x/WARP_SIZE);
+    Ms += ROW_MAX_SIZE * int(threadIdx.x/WARP_SIZE);
+    // load from the global memory to the shared memory
+    #pragma unroll
+    for(int i=bn; i<global_attention_size; i+=32){
+        A_index = (blockIdx.x * row_tile + bm) * N + global_attention[i];
+        As[i] = A[A_index];
+        Ms[i] = 1.0;
+        A[A_index] = -10000.0; // 
+    }
+    #pragma unroll
+    for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
+        // FIXME: there need one more for loop to handle the WARP_SIZE -> BLOCK_W
+        mask_index = block_h * block_w * block_seq + (block_inter_row + bm) * block_w + bn;
+        A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
+        As[global_attention_size + block_w * (block_seq - block_seq_start) + bn] = A[A_index];
+        Ms[global_attention_size + block_w * (block_seq - block_seq_start) + bn] = val_mask[mask_index];
+    }
+    // calculate the softmax in the shared memory
+    // calculate the regMax
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        regMax = max(regMax, As[i]*Ms[i]);
+    }
+    #pragma unroll
+    for (int offset = 16; offset > 0; offset /= 2) {
+        regMax = max(regMax, __shfl_down_sync(FULL_MASK, regMax, offset));
+    }
+    regMax = __shfl_sync(FULL_MASK, regMax, 0);    
+    // calculate the regSum
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        regSum += Ms[i] * expf(As[i]-regMax);
+    }
+    #pragma unroll
+    for (int offset = 16; offset > 0; offset /= 2) {
+        regSum += __shfl_down_sync(FULL_MASK, regSum, offset);
+    }
+    regSum = __shfl_sync(FULL_MASK, regSum, 0);
+    // calculate the corresponding value for each element
+    #pragma unroll
+    for(int i =bn; i<global_attention_size+(block_seq_end-block_seq_start)*block_w; i+=WARP_SIZE){
+        As[i] = expf(As[i] - regMax) / regSum;
+    }
+
+    // Write the results back to the global memory
+    #pragma unroll
+    for (int block_seq = block_seq_start; block_seq < block_seq_end; block_seq++) {
+        /// TODO FIXME: need one more loop from the BLOCK_W to the WARP_SIZE
+        A_index = (blockIdx.x * row_tile + bm) * N + (col[block_seq] * block_w + bn);
+        shared_index = global_attention_size+(block_seq-block_seq_start) * block_w + bn;
+        A[A_index] = As[shared_index] * Ms[shared_index]; 
+    }
+    for(int i=bn; i<global_attention_size; i+=32){
+        A_index = (blockIdx.x * row_tile + bm) * N + global_attention[i];
+        A[A_index] = As[i]; 
+    }
 
 }
 void longformer_mixed_softmax_launch(float * A,
@@ -399,6 +515,39 @@ void longformer_mixed_softmax_launch(float * A,
                                                            global_attention_size
                                                            );
 }
+void longformer_mixed_softmax_launch_v2(float * A,
+                                     int * row,
+                                     int *col,
+                                     float* val_mask,
+                                     int * global_attention,
+                                     float* extra_buffer,
+                                     int block_h,
+                                     int block_w,
+                                     int block_nnz,
+                                     int M,
+                                     int N,
+                                     int head_num,
+                                     int batch_size,
+                                     int global_attention_size
+)
+{
+    const int row_tile=4;
+    const dim3 blockDim(row_tile*32);
+    const dim3 gridDim(M/row_tile, head_num*batch_size);
+    longformer_mixed_softmax_kernel_v2<<<gridDim, blockDim>>>(A,
+                                                           row,
+                                                           col,
+                                                           val_mask,
+                                                           global_attention,
+                                                           extra_buffer,
+                                                           block_h,
+                                                           block_w,
+                                                           block_nnz,
+                                                           row_tile,
+                                                           M, N,
+                                                           global_attention_size
+                                                           );
+}
 void generate_global_attention(int * g, int count, int max_seq_len)
 {
     std::vector<int> vec;
@@ -409,8 +558,8 @@ void generate_global_attention(int * g, int count, int max_seq_len)
     std::shuffle(vec.begin(), vec.end(), rng);
     for(int i=0;i<count;i++)
         g[i] = vec[i];
-    for(int i=0;i<count;i++)
-        printf("%d\n", g[i]);
+    // for(int i=0;i<count;i++)
+    //     printf("%d\n", g[i]);
 }
 int main()
 {
@@ -424,7 +573,7 @@ int main()
     int block_h=32, block_w = 32;
     int global_attention_size = 80;
     // float sparsiy=0.9999;
-    float sparsiy=0.85;
+    float sparsiy=0.9;
     float * data = (float*) malloc(sizeof(float)*M*N*batch_size*head_num);
     int * mask = (int*) malloc(sizeof(int)*M*N);
     int * row = (int*) malloc(sizeof(int)*(M+1));
@@ -466,7 +615,7 @@ int main()
 
     CUDA_SAFE_CALL(cudaEventRecord(time_start));
     for(int runtime=0; runtime<10; runtime++){
-        longformer_mixed_softmax_launch(dA,
+        longformer_mixed_softmax_launch_v2(dA,
                                     d_row,
                                     d_col,
                                     d_val,
