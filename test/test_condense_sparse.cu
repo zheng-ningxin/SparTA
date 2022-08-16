b0c0202b6657e01f11f82741808e6fdb10b9974d
nizhen <nizhen>
2022-08-16 14:59:29 +0900

    update

---
+++ test/test_condense_sparse.cu
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cublas.h>
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <time.h>
+// #include <math>
+#include <algorithm>
+#include <assert.h>
+// CUDA runtime
+#include <cuda.h>
+using namespace std;
+#define OFFSET(row, col, ld) ((row) * ld + col)
+#define FETCH_FLOAT4(pointer) (reinterpret_cast<float4*>(&pointer))[0]
+#define FETCH_UINT32(pointer) (reinterpret_cast<unsigned int*>(&(pointer))[0])
+#define FETCH_UINT4(pointer) (reinterpret_cast<uint4*>(&(pointer))[0])
+#define FETCH_INT4(pointer) (reinterpret_cast<int4*>(&(pointer))[0])
+#define FETCH_INT32(pointer) (reinterpret_cast<int*>(&(pointer))[0])
+#define MAX_BLOCK_THREAD_COUNT 1024
+#define FULL_MASK 0xffffffff
+
+
+#define CUBLAS_SAFE_CALL(func)                                                                  \
+    do                                                                                          \
+    {                                                                                           \
+        cublasStatus_t e = (func);                                                              \
+        if (e != CUBLAS_STATUS_SUCCESS)                                                         \
+        {                                                                                       \
+            std::stringstream safe_call_ss;                                                     \
+            safe_call_ss << "\nerror: " #func " failed with error"                              \
+                         << "\nfile: " << __FILE__ << "\nline: " << __LINE__ << "\nmsg: " << e; \
+            throw std::runtime_error(safe_call_ss.str());                                       \
+        }                                                                                       \
+    } while (0)
+
+#define CUDA_SAFE_CALL(x)                                                                         \
+    do                                                                                            \
+    {                                                                                             \
+        cudaError_t result = (x);                                                                 \
+        if (result != cudaSuccess)                                                                \
+        {                                                                                         \
+            const char *msg = cudaGetErrorString(result);                                         \
+            std::stringstream safe_call_ss;                                                       \
+            safe_call_ss << "\nerror: " #x " failed with error"                                   \
+                         << "\nfile: " << __FILE__ << "\nline: " << __LINE__ << "\nmsg: " << msg; \
+            throw std::runtime_error(safe_call_ss.str());                                         \
+        }                                                                                         \
+    } while (0)
+
+__device__ void warpReduce(volatile int* sdata, int tid) {
+    sdata[tid] += sdata[tid + 32]; 
+    sdata[tid] += sdata[tid + 16]; 
+    sdata[tid] += sdata[tid + 8]; 
+    sdata[tid] += sdata[tid + 4]; 
+    sdata[tid] += sdata[tid + 2]; 
+    sdata[tid] += sdata[tid + 1]; 
+}
+
+__device__ __forceinline__ const int* add_ptr_u(const int* src, int offset)      \
+{                                                                            \
+    const int* dst;                                                            \
+    asm("{                       \n\t"                                       \
+        ".reg .u32 lo,hi,of;     \n\t"                                       \
+        "mul.lo.u32 of, %2, %3;  \n\t"                                       \
+        "mov.b64    {lo,hi}, %1; \n\t"                                       \
+        "add.cc.u32  lo,lo,  of; \n\t"                                       \
+        "addc.u32    hi,hi,  0;  \n\t"                                       \
+        "mov.b64 %0, {lo,hi};    \n\t"                                       \
+        "}" : "=l"(dst) : "l"(src), "r"(offset), "r"((int)sizeof(*src)));    \
+    return dst;                                                              \
+}
+
+__device__ __forceinline__ const float* add_ptr_f(const float* src, int offset)      \
+{                                                                            \
+    const float* dst;                                                            \
+    asm("{                       \n\t"                                       \
+        ".reg .u32 lo,hi,of;     \n\t"                                       \
+        "mul.lo.u32 of, %2, %3;  \n\t"                                       \
+        "mov.b64    {lo,hi}, %1; \n\t"                                       \
+        "add.cc.u32  lo,lo,  of; \n\t"                                       \
+        "addc.u32    hi,hi,  0;  \n\t"                                       \
+        "mov.b64 %0, {lo,hi};    \n\t"                                       \
+        "}" : "=l"(dst) : "l"(src), "r"(offset), "r"((int)sizeof(*src)));    \
+    return dst;                                                              \
+}
+
+__device__ __forceinline__ float2  _add(float2 x, float2 y) { float2 res; res.x = x.x + y.x; res.y = x.y + y.y; return res; }
+
+int main()
+{
+    int M, K, N;
+    M = 1024;
+    K = 1024;
+    N = 1024;
+    cudaEvent_t time_start, time_end;
+    float msceTotal = 0;
+    
+    return 0;
+}
