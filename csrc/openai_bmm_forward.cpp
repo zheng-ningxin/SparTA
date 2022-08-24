a51370cd7231b60f5e48676beac1a211f9394332
nizhen <nizhen>
2022-08-24 14:36:26 +0900

    update

--- csrc/openai_bmm_forward.cpp
+++ csrc/openai_bmm_forward.cpp
@@ -11,7 +11,21 @@ at::Tensor openai_bmm_32_64_32(
     int batch_size,
     int block_nnz);
 
+at::Tensor openai_bmm_32_64_32_condense(
+    torch::Tensor row_ptr,
+    torch::Tensor col_idx,
+    torch::Tensor values,
+    torch::Tensor B,
+    int M,
+    int K,
+    int N,
+    int block_h,
+    int block_w,
+    int batch_size,
+    int block_nnz);
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     m.def("forward", &openai_bmm_32_64_32, "openai_bmm_32_64_32 sparse forward");
+    m.def("forward_condense", &openai_bmm_32_64_32_condense, "openai_bmm_32_64_32_condense forward");
 }
