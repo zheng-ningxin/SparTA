b7ce151246081e85c2a788676fc563c715c51142
nizhen <nizhen>
2022-08-30 12:06:55 +0900

    update dim sparse linear

--- sparta/opset/dim_dynamic_sparse_linear.py
+++ sparta/opset/dim_dynamic_sparse_linear.py
@@ -10,14 +10,29 @@ class DimDynamicLinear(torch.nn.Module):
         self.in_or_out = in_or_out
 
     def forward(self, data, mask):
-        b_mask = mask.to(torch.bool)
+        assert mask.is_cuda
+        # import ipdb; ipdb.set_trace()
+        _pos = mask.nonzero(as_tuple=True)[0]
+        
         if self.in_or_out == 0:
             # out
             re = F.linear(data,
-                     self.weight[b_mask],
-                     None if self.bias is None else self.bias[b_mask])
+                     torch.index_select(self.weight, 0, _pos),
+                     None if self.bias is None else torch.index_select(self.bias, 0, _pos))
         else:
-            re = F.linear(data,
-                     self.weight[:, b_mask],
+            re = F.linear(data, torch.index_select(self.weight, 1, _pos),
                      self.bias)
         return re
+    # def forward(self, data, mask):
+    #     assert mask.is_cuda
+    #     b_mask = mask.to(torch.bool)
+    #     if self.in_or_out == 0:
+    #         # out
+    #         re = F.linear(data,
+    #                  self.weight[b_mask],
+    #                  None if self.bias is None else self.bias[b_mask])
+    #     else:
+    #         re = F.linear(data,
+    #                  self.weight[:, b_mask],
+    #                  self.bias)
+    #     return re
--- test/test_dim_sparse_linear.py
+++ test/test_dim_sparse_linear.py
@@ -22,8 +22,34 @@ def measure_time(module, data):
     avg_t = (t_end-t_start)*1000/runtime
     print('Dense Time(ms):', avg_t)
     
+def measure_time_sparse_profile(module, data, mask):
+    runtime = 15000
+    in_data = data.clone().detach()
+    in_data.requires_grad_()
+    with torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=20),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/dim_sparse_linear_v2'),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True
+    ) as prof:
+        #warmup
+        for i in range(100):
+            re = module(in_data, mask)
+        _grad = torch.rand_like(re)
+        torch.cuda.synchronize()
+        t_start = time.time()
+        for i in range(runtime):
+            re = module(in_data, mask)
+            re.backward(_grad)
+            prof.step()
+        torch.cuda.synchronize()
+        t_end = time.time()
+        avg_t = (t_end-t_start)*1000/runtime
+        print('Sparse Time(ms):', avg_t)
+
 def measure_time_sparse(module, data, mask):
-    runtime = 5000
+    runtime = 15000
     in_data = data.clone().detach()
     in_data.requires_grad_()
     #warmup
@@ -46,13 +72,14 @@ if __name__ == '__main__':
     K = 1024
     N = 1024
     
-    for sparsity in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]:
+    # for sparsity in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]:
+    for sparsity in [ 0.95]:
         ori_linear = torch.nn.Linear(K, N).cuda()
         d_linear = DimDynamicLinear(ori_linear, 0)
         out_c_w = torch.rand(N).cuda()
         c_mask = (out_c_w > sparsity).to(torch.int32)
         print('Sparsity Ratio:', torch.sum(c_mask)/c_mask.numel())
         data = torch.rand(M, K).cuda()
-        measure_time(ori_linear, data)
-        measure_time_sparse(d_linear, data, c_mask)
+        # measure_time(ori_linear, data)
+        measure_time_sparse_profile(d_linear, data, c_mask)
         
