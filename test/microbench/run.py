586e611647b8075c44dadb97998c65d3b82f7abb
nizhen <nizhen>
2022-11-24 23:18:40 +0900

    micro bench run.py

---
+++ test/microbench/run.py
@@ -0,0 +1,21 @@
+import os
+
+os.makedirs('log', exist_ok=True)
+
+sparsity = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]
+shape = [(4096, 4096, 4096), (4096, 768, 3072)]
+blocksize = [(1, 32), (64, 1), (64, 32)]
+
+def run_cusparse():
+    for s in sparsity:
+        for m,k,n in shape:
+            for block_h, block_w in blocksize:
+                try:
+                    print(f'cusparse {s} {m} {k} {n} {block_h} {block_w}')
+                    os.system(f'./cusparse {s} {m} {k} {n} {block_h} {block_w} > log/cusparse_{s}_{m}_{k}_{n}_{block_h}_{block_w}.log')
+                except Exception as err:
+                    print(err)
+
+if __name__ == '__main__':
+    run_cusparse()
+
