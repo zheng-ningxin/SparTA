2452465f381318ca97fecbc55324f23dc9e25302
nizhen <nizhen>
2022-11-26 11:55:57 +0900

    updaet

--- test/microbench/test_openai_bmm.py
+++ test/microbench/test_openai_bmm.py
@@ -14,45 +14,39 @@ def convert_to_full_mask(block_layout, block_size):
 
 if __name__ == '__main__':
     batchsize = 1
-    sparsity_ratio, M, K, N = float(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
+    sparsity_ratio, M, K, N, block_h, block_w = float(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6])
     # for M, K, N in [(4096, 4096, 4096), (4096, 768, 4096)]:
-    block_h = 32
-    block_w = 64
-    RUNTIME = 2000
-        
-        # for sparsity_ratio in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]:
-        # for sparsity_ratio in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]:
-    with open('stil.log', 'a') as f:
-        f.write(f'MKN: {M} {K} {N} sparsity:{sparsity_ratio} \n')
 
-    ########################################################################
-    # measure the dense baseline time
-    A = torch.rand(batchsize, M, K).cuda()
-    B = torch.rand(K, N).cuda()
-    torch.cuda.synchronize()
-    t_start = time.time()
-    for _ in range(RUNTIME):
-        C = torch.matmul(A, B)
-    torch.cuda.synchronize()
-    t_end = time.time()
-    with open('stil.log', 'a') as f:
-        t_re = (t_end-t_start)*1000/RUNTIME
-        f.write(f'Dense time baseline latency(ms):{t_re} \n')
-    
+    RUNTIME = 2000
+    tile_block_h = 32
+    tile_block_w = 64 
+    n_block_h = M // block_h
+    n_block_w = K // block_w
+    block_wise_weight = torch.rand(n_block_h, n_block_w, dtype=torch.float32).cuda()
+    block_mask = (block_wise_weight > sparsity_ratio).to(torch.int32)
+    full_mask = convert_to_full_mask(block_mask, (block_h, block_w))
+    device = torch.device('cuda')
+   
+    conv=torch.nn.Conv2d(1, 1, (tile_block_h, tile_block_w), (tile_block_h, tile_block_w), bias=False).cuda()
+    conv.eval()
+    conv.weight.data[:] = 1
+    mask = full_mask.view(1, 1, full_mask.size(0), full_mask.size(1)).to(torch.float32)
+    block_mask = conv(mask)
+    layout = (block_mask.view(M//tile_block_h, N//tile_block_w)>0).to(torch.int32)
     #######################################################################
     # original openai sparse kernel
     A = torch.rand(batchsize, M, K).cuda()
     A_copy = A.clone().detach()
     B = torch.rand(batchsize, K, N).cuda()
     mask = torch.ones(M, K).cuda()
-    block_wise_weight = torch.rand(M//block_h, K//block_w, dtype=torch.float32).cuda()
-    block_mask = (block_wise_weight > sparsity_ratio).to(torch.int32)
+    block_mask = layout
     # print('Block-wise sparsity ratio:', torch.sum(block_mask)/block_mask.numel())
-    full_mask = convert_to_full_mask(block_mask, (block_h, block_w))
+    full_mask = convert_to_full_mask(block_mask, (tile_block_h, tile_block_w))
+    print('real sparsity ratio: {}\n'.format(torch.sum(full_mask)/full_mask.numel()))
     A *= full_mask
     ref_out = torch.einsum('bmk,bkn->bmn',A, B)
     converter_1 = BcsrConverter()
-    row_ptr, col_idx, row_pos, vals = converter_1(full_mask, A, block_h, block_w)
+    row_ptr, col_idx, row_pos, vals = converter_1(full_mask, A, tile_block_h, tile_block_w)
     block_nnz = row_ptr[M//block_h]
     out = openai_bmm_cpp.forward(row_ptr, col_idx, vals, B, M, K, N, batchsize, block_nnz)
     if not torch.allclose(out, ref_out, rtol=1e-04, atol=1e-03):
@@ -65,113 +59,5 @@ if __name__ == '__main__':
         out = openai_bmm_cpp.forward(row_ptr, col_idx, vals, B, M, K, N, batchsize, block_nnz)
     torch.cuda.synchronize()
     t_end = time.time()
-    with open('stil.log', 'a') as f:
-        t_re = (t_end-t_start)*1000/RUNTIME
-        f.write(f'Original openai bmm latency(ms):{t_re} \n')
-
-    ###########################################################################
-    # following is the condense matmul computation
-    B = torch.rand(batchsize, K, N).cuda()
-    A = torch.rand(batchsize, M, K).cuda()    
-    new_block_h = block_h
-    new_block_w = 1
-    converter_2 = BcsrConverter(True)
-    t_block_wise_weight = torch.rand(M//new_block_h, K//new_block_w, dtype=torch.float32).cuda()
-    t_block_mask = (t_block_wise_weight > sparsity_ratio).to(torch.int32)
-    # print("Block-wise sparsity ratio:", torch.sum(t_block_mask)/t_block_mask.numel())
-    t_full_mask = convert_to_full_mask(t_block_mask, (new_block_h, new_block_w))
-    A *= t_full_mask
-    ref_out = torch.einsum('bmk,bkn->bmn',A, B)
-    # print(torch.squeeze(A).size())
-    t_row_ptr, t_col_idx, t_row_pos, t_vals = converter_2(t_full_mask, torch.squeeze(A), new_block_h, new_block_w)
-    t_block_nnz = t_row_ptr[M//new_block_h]
-    condense_out = openai_bmm_cpp.forward_condense(t_row_ptr, t_col_idx, t_vals, B, M, K, N, new_block_h, new_block_w, batchsize, t_block_nnz)
-    assert torch.allclose(condense_out, ref_out, rtol=1e-03, atol=1e-03)
-    torch.cuda.synchronize()
-    t_start = time.time()
-    for _ in range(RUNTIME):
-        condense_out = openai_bmm_cpp.forward_condense(t_row_ptr, t_col_idx, t_vals, B, M, K, N, new_block_h, new_block_w, batchsize, t_block_nnz)
-    torch.cuda.synchronize()
-    t_end = time.time()
-    with open('stil.log', 'a') as f:
-        t_re = (t_end-t_start)*1000/RUNTIME
-        f.write(f'Condense openai bmm latency(ms): {t_re} \n')
-
-    
-    #############################################################################
-    # following is the condense module on the M dimension
-    A = torch.rand(batchsize, M, K).cuda()
-    B = torch.rand(batchsize, K, N).cuda()
-    # A[:,:32] = 1
-    new_block_h = 1
-    new_block_w = 64
-    block_wise_weight = torch.rand(M//new_block_h, K//new_block_w, dtype=torch.float32).cuda()
-    block_mask = (block_wise_weight > sparsity_ratio).to(torch.int32)
-    # block_mask[:] = 1
-    # block_mask[:32] = 1
-    # block_mask[1:10] = 0
-    # print("Block-wise sparsity ratio:", torch.sum(block_mask)/block_mask.numel())
-    # import ipdb; ipdb.set_trace()
-    full_mask = convert_to_full_mask(block_mask, (new_block_h, new_block_w))
-    A *= full_mask
-    ref_out = torch.einsum('bmk,bkn->bmn',A, B)
-    m_csr_row, m_csr_col, m_csr_val = convert_bcsr(full_mask.t(), torch.squeeze(A).t(), new_block_w, new_block_h)
-    m_csr_row, m_csr_col, m_csr_val = m_csr_row.cuda(), m_csr_col.cuda(), m_csr_val.cuda()
-    m_block_nnz = m_csr_row[K//new_block_w].item()
-    # print(m_block_nnz)
-    # import ipdb; ipdb.set_trace()
-    condense_out_m = openai_bmm_cpp.forward_condense_m(m_csr_row, m_csr_col, m_csr_val, B, M, K, N, new_block_h, new_block_w, batchsize, m_block_nnz)
-    flag = torch.allclose(condense_out_m, ref_out, rtol=1e-04, atol=1e-03)
-    if not flag:
-        import ipdb; ipdb.set_trace()
-        print("Correctness Failed!")
-    torch.cuda.synchronize()
-    t_start = time.time()
-    for _ in range(RUNTIME):
-        condense_out_m = openai_bmm_cpp.forward_condense_m(m_csr_row, m_csr_col, m_csr_val, B, M, K, N, new_block_h, new_block_w, batchsize, m_block_nnz)
-    torch.cuda.synchronize()
-    t_end = time.time()
-    with open('stil.log', 'a') as f:
-        t_re = (t_end-t_start)*1000/RUNTIME
-        f.write(f'Condense openai bmm on dim m latency(ms): {t_re} \n')
-
-
-    # print('Condense openai bmm on dim m latency(ms):', (t_end-t_start)*1000/RUNTIME)
-    #########################################################################
-    A = torch.rand(batchsize, M, K).cuda()
-    B = torch.rand(batchsize, K, N).cuda()
-    # A[:,:32] = 1
-    new_block_h = 1
-    new_block_w = 64
-    block_wise_weight = torch.rand(M//new_block_h, K//new_block_w, dtype=torch.float32).cuda()
-    block_mask = (block_wise_weight > sparsity_ratio).to(torch.int32)
-    # block_mask[:] = 1
-    # block_mask[:32] = 1
-    # block_mask[1:10] = 0
-    # print("Block-wise sparsity ratio:", torch.sum(block_mask)/block_mask.numel())
-    # import ipdb; ipdb.set_trace()
-    full_mask = convert_to_full_mask(block_mask, (new_block_h, new_block_w))
-    A *= full_mask
-    ref_out = torch.einsum('bmk,bkn->bmn',A, B)
-    m_csr_row, m_csr_col, m_csr_val = convert_bcsr(full_mask.t(), torch.squeeze(A).t(), new_block_w, new_block_h)
-    m_csr_row, m_csr_col, m_csr_val = m_csr_row.cuda(), m_csr_col.cuda(), m_csr_val.cuda()
-    m_block_nnz = m_csr_row[K//new_block_w].item()
-    # print(m_block_nnz)
-    # import ipdb; ipdb.set_trace()
-    condense_out_m = openai_bmm_cpp.forward_condense_m_v2(m_csr_row, m_csr_col, m_csr_val, B, M, K, N, new_block_h, new_block_w, batchsize, m_block_nnz)
-    flag = torch.allclose(condense_out_m, ref_out, rtol=1e-04, atol=1e-03)
-    if not flag:
-        import ipdb; ipdb.set_trace()
-        print("Correctness Failed!")
-    torch.cuda.synchronize()
-    t_start = time.time()
-    for _ in range(RUNTIME):
-        condense_out_m = openai_bmm_cpp.forward_condense_m_v2(m_csr_row, m_csr_col, m_csr_val, B, M, K, N, new_block_h, new_block_w, batchsize, m_block_nnz)
-    torch.cuda.synchronize()
-    t_end = time.time()
-    with open('stil.log', 'a') as f:
-        t_re = (t_end-t_start)*1000/RUNTIME
-        f.write(f'Condense openai bmm on dim m latency v2(ms): {t_re} \n')
-        f.write("##################################\n\n")
-    # print('Condense openai bmm on dim m latency v2(ms):', (t_end-t_start)*1000/RUNTIME)
-    # print('##############################\n\n')
+    t_re = (t_end-t_start)*1000/RUNTIME
+    print(f"Time= {t_re} ms\n")
