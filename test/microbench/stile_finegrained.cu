a4ca87136c96d7e27d8173aa40ad481d076809c7
nizhen <nizhen>
2022-12-02 13:37:07 +0900

    stile finegrained unfinished

--- test/microbench/stile_finegrained.cu
+++ test/microbench/stile_finegrained.cu
@@ -350,6 +350,85 @@ void FINEGRAINED_CONDESE_V3(int *csr_row, int * csr_col, float* csr_val, float *
     FINEGRAINED_CONDENSE_KERNEL_V3<BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N><<<gridDim, blockDim>>>(csr_row, csr_col, csr_val, B, C, M, K, N);
 
 }
+
+
+
+template <
+    const int BLOCK_SIZE_M,
+    const int BLOCK_SIZE_K,
+    const int BLOCK_SIZE_N
+>
+__global__ void FINEGRAINED_CONDENSE_KERNEL_V4(const int* __restrict__  csr_row, const int* __restrict__  csr_col, const float* __restrict__  csr_val,  float* __restrict__  B, float* __restrict__  C, const int M, const int K, const int N){
+    
+
+    int by = blockIdx.y;
+    int bx = blockIdx.x;
+    int tid = threadIdx.x;
+    const int padding = 1;
+
+    __shared__ int Is[BLOCK_SIZE_M * BLOCK_SIZE_K];
+    __shared__ float Vs[BLOCK_SIZE_M * BLOCK_SIZE_K];
+    const int n_threads_per_row = 32;
+    int ty = tid/n_threads_per_row;
+    int tx = tid%n_threads_per_row;
+    int row_id = by * BLOCK_SIZE_M + ty;
+    int index_start = csr_row[row_id];
+    int index_end = csr_row[row_id+1];
+    const int n_thread_per_row = BLOCK_SIZE_N;
+
+    #pragma unroll
+    // for(int n_round=0; n_round<N_TILE_SIZE/BLOCK_SIZE_N; n_round++){
+    float sum = 0;
+    int n_start = bx * BLOCK_SIZE_N;
+    // int n_end = n_start + BLOCK_SIZE_N;
+    #pragma unroll
+    for(int k_round=0; k_round< (index_end-index_start-1+BLOCK_SIZE_K)/BLOCK_SIZE_K; k_round++){
+        // load the A to the shared memory
+        int k_start = index_start + k_round * BLOCK_SIZE_K;
+        // int k_end = min(k_start+ BLOCK_SIZE_K, index_end);
+        int k_end = k_start + BLOCK_SIZE_K;
+        for(int _pos=tx+k_start; _pos<k_end; _pos+=n_thread_per_row){
+            if(_pos<index_end){
+                Is[ty*BLOCK_SIZE_K + _pos-k_start] = csr_col[_pos];
+                Vs[ty*BLOCK_SIZE_K + _pos-k_start] = csr_val[_pos];
+            }else{
+                Vs[ty*BLOCK_SIZE_K + _pos-k_start] = 0;
+            }
+        }
+        __syncthreads();
+        // load B to the shared memory
+        // #pragma unroll
+        // for(int _pos=ty; _pos<min(index_end-k_start, BLOCK_SIZE_K); _pos+=n_stride){
+        //     int k_offset = Is[_pos];
+        //     FETCH_FLOAT4(Bs[OFFSET(_pos, tx*4, BLOCK_SIZE_N)]) = 
+        //         FETCH_FLOAT4(B[OFFSET(k_offset, n_start+tx*4, N)]);
+        // }
+        // __syncthreads();
+        // computation the spmv
+        #pragma unroll
+        for(int i=0;i<BLOCK_SIZE_K;i++){
+            int k_offset = Is[ty*BLOCK_SIZE_K+i];
+            sum += Vs[ty*BLOCK_SIZE_K + i]*B[OFFSET(k_offset, n_start + tx, N)];
+        }
+
+    }
+    // write backto C
+    C[OFFSET(row_id, n_start+tx, N)] = sum;
+    // }
+
+}
+void FINEGRAINED_CONDESE_V4(int *csr_row, int * csr_col, float* csr_val, float * B, float* C, int M, int K, int N)
+{
+    const int BLOCK_SIZE_M = 32;
+    const int BLOCK_SIZE_N = 32;
+    const int BLOCK_SIZE_K = 32;
+    dim3 gridDim(N/BLOCK_SIZE_N, M/BLOCK_SIZE_M);
+    dim3 blockDim(BLOCK_SIZE_M*BLOCK_SIZE_N);
+    FINEGRAINED_CONDENSE_KERNEL_V2<BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N><<<gridDim, blockDim>>>(csr_row, csr_col, csr_val, B, C, M, K, N);
+
+}
+
+
 int convert_csr(float * ptr, int32_t row, int32_t col, int32_t * row_idx, int32_t * col_idx, float * values)
 {
     auto v_row_idx = std::make_shared<vector<int32_t>>();
