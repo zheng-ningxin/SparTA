98d34ae35933d7ca590676e49a4fe13553a26b9d
nizhen <nizhen>
2022-12-07 15:21:37 +0900

    update moe

--- csrc/moe_sparse_forward.cpp
+++ csrc/moe_sparse_forward.cpp
@@ -6,14 +6,20 @@ at::Tensor moe_sparse_forward(
     torch::Tensor tokens,
     torch::Tensor weight,
     torch::Tensor router_index, // given by the router function
-    torch::Tensor sparse_index, 
-    int n_expert,
-    int in_hidden,
-    int out_hidden
+    torch::Tensor sparse_index
 );
 
+void moe_sparse_convert_index(
+    torch::Tensor tokens,
+    torch::Tensor weight,
+    torch::Tensor router_index,
+    torch::Tensor sparse_index,
+    torch::Tensor expert_count
+);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     m.def("forward", &moe_sparse_forward, "dynamic sparse forward function of MOE");
+    m.def("convert_index", &moe_sparse_convert_index, "dynamic sparse index construction function of MOE");
+
 }
