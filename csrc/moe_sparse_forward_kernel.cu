049b0b4329464bfe92a5f18b455cab57b604d3af
nizhen <nizhen>
2022-12-07 23:10:00 +0900

    sparse moe

--- csrc/moe_sparse_forward_kernel.cu
+++ csrc/moe_sparse_forward_kernel.cu
@@ -86,7 +86,7 @@ __device__ __forceinline__ const float* add_ptr_f(const float* src, int offset) 
 __device__ __forceinline__ float2  _add(float2 x, float2 y) { float2 res; res.x = x.x + y.x; res.y = x.y + y.y; return res; }
 
 
-__global__ void BATCH_BLOCK_SPARSE_MATMUL(float* A_val, int* A_row, int* A_col, float*B, float*C, int GLOBAL_M, int GLOBAL_K, int GLOBAL_N, int SPARSE_VAL_SIZE)
+__global__ void BATCH_BLOCK_SPARSE_MATMUL(float* tokens, int* sparse_index, int* expert_count, float*B, float*C, int GLOBAL_M, int GLOBAL_K, int GLOBAL_N, const int TMAX)
 {
     /*
     description:
@@ -104,9 +104,8 @@ __global__ void BATCH_BLOCK_SPARSE_MATMUL(float* A_val, int* A_row, int* A_col, 
     const int K = GLOBAL_K;
     const int N = GLOBAL_N;
 
-    A_val += SPARSE_VAL_SIZE * blockIdx.z;
+    int exp_id = blockIdx.z;
     B += K * N * blockIdx.z;
-    C += M * N * blockIdx.z;
 
     assert(blockDim.x % 32 == 0);
     uint n_warp = 8; // blockDim.x / 32
@@ -116,8 +115,8 @@ __global__ void BATCH_BLOCK_SPARSE_MATMUL(float* A_val, int* A_row, int* A_col, 
 
     assert(BLOCK_SIZE_M == BLOCK_SIZE_N);
     __shared__ float fShare[65 * 32 * 2];
+    __shared__ int m_index[BLOCK_SIZE_M];
     char* bShare = (char*)fShare;
-
     uint tid = threadIdx.x;
     uint bx = blockIdx.x; // N
     uint by = blockIdx.y; // M
@@ -127,13 +126,12 @@ __global__ void BATCH_BLOCK_SPARSE_MATMUL(float* A_val, int* A_row, int* A_col, 
     assert(THREAD_SIZE_K % 16 == 0);
     uint k = tx * 4;
 
-    uint ori_offsetB00 = tid / (BLOCK_SIZE_N/4) * N + by * BLOCK_SIZE_N + (tid % (BLOCK_SIZE_N/4)) * 4;
+    uint ori_offsetB00 = tid / (BLOCK_SIZE_N/4) * N + bx * BLOCK_SIZE_N + (tid % (BLOCK_SIZE_N/4)) * 4;
     uint ori_offsetB16 = ori_offsetB00 + N * 32;
     uint storB = (tid * 4 + tid / (BLOCK_SIZE_N/4) / 4 *2) * 4; // (tid *4 + tid / (BLOCK_SIZE_N/4) / 4 * 2)*4
 
     // B is stored in sparse format, thus, should be dealt with differently
-    uint offsetA00 = A_row[bx] * BLOCK_SIZE_M * BLOCK_SIZE_K + ty * BLOCK_SIZE_K + k;
-    uint offsetA16 = offsetA00 + BLOCK_SIZE_K * 16;
+
 
     uint tid224 = tid & 224;
     uint storAB = (tx * 32 * 4 + ty + tx * 2) * 4;
@@ -155,133 +153,141 @@ __global__ void BATCH_BLOCK_SPARSE_MATMUL(float* A_val, int* A_row, int* A_col, 
 
     // bx means in index of this thread block on N dimonsion
     // index_start and index_end is block index on column
-    int index_start = A_row[bx], index_end = A_row[bx+1];
-    // if(blockIdx.z == 1 && blockIdx.x==0 && blockIdx.y == 0){
-    //     printf("bx:%d by%d bz:%d index_start:%d index_end:%d Sparse_VAL_SIZE:%d\n", blockIdx.x, blockIdx.y, blockIdx.z, index_start, index_end, SPARSE_VAL_SIZE);
-    // }
-    for(int bcsr_col_idx = index_start; bcsr_col_idx < index_end; bcsr_col_idx += 1)
-    {
-        uint offsetB00 = ori_offsetB00 + 64 * A_col[bcsr_col_idx] * N;
-        uint offsetB16 = ori_offsetB16 + 64 * A_col[bcsr_col_idx] * N;
+    int n_token = expert_count[exp_id];
+    int index_start = exp_id * TMAX + by * BLOCK_SIZE_M;
+    int index_end = min(index_start + BLOCK_SIZE_M, exp_id*TMAX+n_token);
 
-        float4 a00 = {0}, a16 = {0};
-        float4 b00 = {0}, b16 = {0};
-        a00 = __ldg((const float4*)(add_ptr_f(A_val, offsetA00)));
-        a16 = __ldg((const float4*)(add_ptr_f(A_val, offsetA16)));
-        b00 = __ldg((const float4*)(add_ptr_f(B, offsetB00)));
-        b16 = __ldg((const float4*)(add_ptr_f(B, offsetB16)));
+    if(index_start<index_end){
+        // load the m-dimension index to the shared memory
+        if(tid<index_end-index_start ){
+            m_index[tid] = sparse_index[index_start+tid];
+        }
+        __syncthreads();
+        uint ori_offsetA00 = m_index[ty] * K + k;
+        uint ori_offsetA16 = m_index[ty+16] * K + k;
+        for(int k_seq=0; k_seq < (int) ( K / 64) ; k_seq++){
+            uint offsetB00 = ori_offsetB00 + 64 * k_seq * N;
+            uint offsetB16 = ori_offsetB16 + 64 * k_seq * N;
+            uint offsetA00 = ori_offsetA00 + 64 * k_seq;
+            uint offsetA16 = ori_offsetA16 + 64 * k_seq;
+            float4 a00 = {0}, a16 = {0};
+            float4 b00 = {0}, b16 = {0};
+            if(ty<index_end-index_start)
+                a00 = __ldg((const float4*)(add_ptr_f(tokens, offsetA00)));
+            if(ty+16<index_end-index_start)
+                a16 = __ldg((const float4*)(add_ptr_f(tokens, offsetA16)));
+            b00 = __ldg((const float4*)(add_ptr_f(B, offsetB00)));
+            b16 = __ldg((const float4*)(add_ptr_f(B, offsetB16)));
 
-        offsetA00 += BLOCK_SIZE_M * BLOCK_SIZE_K;
-        offsetA16 += BLOCK_SIZE_M * BLOCK_SIZE_K;
+
+            __syncthreads();
+
+            *(float*)&bShare[storAB + (0*32 +  0 + 0*65*32)*4] = a00.x;
+            *(float*)&bShare[storAB + (1*32 +  0 + 0*65*32)*4] = a00.y;
+            *(float*)&bShare[storAB + (2*32 +  0 + 0*65*32)*4] = a00.z;
+            *(float*)&bShare[storAB + (3*32 +  0 + 0*65*32)*4] = a00.w;
+            *(float*)&bShare[storAB + (0*32 + 16 + 0*65*32)*4] = a16.x;
+            *(float*)&bShare[storAB + (1*32 + 16 + 0*65*32)*4] = a16.y;
+            *(float*)&bShare[storAB + (2*32 + 16 + 0*65*32)*4] = a16.z;
+            *(float*)&bShare[storAB + (3*32 + 16 + 0*65*32)*4] = a16.w;
+
+            *(float*)&bShare[storB + (1*65*32)*4] = b00.x;
+            *(float*)&bShare[storB + (1*65*32 + 1)*4] = b00.y;
+            *(float*)&bShare[storB + (1*65*32 + 2)*4] = b00.z;
+            *(float*)&bShare[storB + (1*65*32 + 3)*4] = b00.w;
+            *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32)*4] = b16.x;
+            *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 1)*4] = b16.y;
+            *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 2)*4] = b16.z;
+            *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 3)*4] = b16.w;
+            __syncthreads();
+
+            float regA[8], regB[4];
+            #pragma unroll
+            for (int j = 0; j < 4; j++)
+            {
+                // fetch outer product data
+                *(float4*)&regA[0] = *(float4*)&bShare[loadA + (32*j +  0)*4];
+                *(float4*)&regA[4] = *(float4*)&bShare[loadA + (32*j + 16)*4];
+                *(float4*)&regB[0] = *(float4*)&bShare[loadB + (32*j + 65*32)*4];
+
+                for (int i = 0; i < 8; i++)
+                    for (int j = 0; j < 4; j++)
+                        regC[i][j] += regA[i] * regB[j];
+            }
+            #pragma unroll
+            for (int j = 4; j < 8; j++)
+            {
+                *(float2*)&regA[0] = *(float2*)&bShare[loadA + (32*j +  0 + (j/4)*2)*4];
+                *(float2*)&regA[2] = *(float2*)&bShare[loadA + (32*j +  2 + (j/4)*2)*4];
+                *(float2*)&regA[4] = *(float2*)&bShare[loadA + (32*j + 16 + (j/4)*2)*4];
+                *(float2*)&regA[6] = *(float2*)&bShare[loadA + (32*j + 18 + (j/4)*2)*4];
+                *(float2*)&regB[0] = *(float2*)&bShare[loadB + (32*j +  0 + (j/4)*2 + 65*32)*4];
+                *(float2*)&regB[2] = *(float2*)&bShare[loadB + (32*j +  2 + (j/4)*2 + 65*32)*4];
+
+                for (int i = 0; i < 8; i++)
+                    for (int j = 0; j < 4; j++)
+                        regC[i][j] += regA[i] * regB[j];
+            }
+        }
+        
+        asm volatile ("mov.u32 %0, %tid.x;"   : "=r"(tid)   :);
+        asm volatile ("mov.u32 %0, %ctaid.x;" : "=r"(bx)   :);
+        asm volatile ("mov.u32 %0, %ctaid.y;" : "=r"(by) :);
+
+        ty = ((tid & 16) >> 3) + (tid & 1);
+        tx = ((tid >> 1) & 7) + ((tid & 224) >> 2) + (ty << 2);
+
+        uint storC = ty*32*8*4 + tx*4;
+
+        tx = tid % 16;
+        ty = tid / 16;
+
+        uint readC = ty*32*8 + tx*2 + ((tid & 192)>>2);
+
+        // C should be row major
+        // C += (bx * BLOCK_SIZE_M + ty) * N + (by * BLOCK_SIZE_N + tx * 2);
 
         __syncthreads();
-        // if(blockIdx.z == 1 && blockIdx.x==0 && blockIdx.y == 0){
-        //     printf("a00.x:%f a00.y:%f, a16.x:%f a16.y:%f\n", a00.x, a00.y, a16.x, a16.y);
-        //     printf("b00.x:%f b00.y:%f, b16.x:%f b16.y:%f\n", b00.x, b00.y, b16.x, b16.y);
-        // }
-        *(float*)&bShare[storAB + (0*32 +  0 + 0*65*32)*4] = a00.x;
-        *(float*)&bShare[storAB + (1*32 +  0 + 0*65*32)*4] = a00.y;
-        *(float*)&bShare[storAB + (2*32 +  0 + 0*65*32)*4] = a00.z;
-        *(float*)&bShare[storAB + (3*32 +  0 + 0*65*32)*4] = a00.w;
-        *(float*)&bShare[storAB + (0*32 + 16 + 0*65*32)*4] = a16.x;
-        *(float*)&bShare[storAB + (1*32 + 16 + 0*65*32)*4] = a16.y;
-        *(float*)&bShare[storAB + (2*32 + 16 + 0*65*32)*4] = a16.z;
-        *(float*)&bShare[storAB + (3*32 + 16 + 0*65*32)*4] = a16.w;
-
-        *(float*)&bShare[storB + (1*65*32)*4] = b00.x;
-        *(float*)&bShare[storB + (1*65*32 + 1)*4] = b00.y;
-        *(float*)&bShare[storB + (1*65*32 + 2)*4] = b00.z;
-        *(float*)&bShare[storB + (1*65*32 + 3)*4] = b00.w;
-        *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32)*4] = b16.x;
-        *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 1)*4] = b16.y;
-        *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 2)*4] = b16.z;
-        *(float*)&bShare[storB + (32*32 + 8*2 + 1*65*32 + 3)*4] = b16.w;
+        *(float4*)&fShare[storC + 0*32*8] = *(float4*)regC[0];
+        *(float4*)&fShare[storC + 1*32*8] = *(float4*)regC[1];
+        *(float4*)&fShare[storC + 2*32*8] = *(float4*)regC[2];
+        *(float4*)&fShare[storC + 3*32*8] = *(float4*)regC[3];
         __syncthreads();
 
-        float regA[8], regB[4];
-        #pragma unroll
-        for (int j = 0; j < 4; j++)
-        {
-            // fetch outer product data
-            *(float4*)&regA[0] = *(float4*)&bShare[loadA + (32*j +  0)*4];
-            *(float4*)&regA[4] = *(float4*)&bShare[loadA + (32*j + 16)*4];
-            *(float4*)&regB[0] = *(float4*)&bShare[loadB + (32*j + 65*32)*4];
+        float2 c2[8];
+        for (int i = 0; i < 8; i++)
+            c2[i] = *(float2*)&fShare[readC + i*32];
 
-            for (int i = 0; i < 8; i++)
-                for (int j = 0; j < 4; j++)
-                    regC[i][j] += regA[i] * regB[j];
-        }
-        #pragma unroll
-        for (int j = 4; j < 8; j++)
-        {
-            *(float2*)&regA[0] = *(float2*)&bShare[loadA + (32*j +  0 + (j/4)*2)*4];
-            *(float2*)&regA[2] = *(float2*)&bShare[loadA + (32*j +  2 + (j/4)*2)*4];
-            *(float2*)&regA[4] = *(float2*)&bShare[loadA + (32*j + 16 + (j/4)*2)*4];
-            *(float2*)&regA[6] = *(float2*)&bShare[loadA + (32*j + 18 + (j/4)*2)*4];
-            *(float2*)&regB[0] = *(float2*)&bShare[loadB + (32*j +  0 + (j/4)*2 + 65*32)*4];
-            *(float2*)&regB[2] = *(float2*)&bShare[loadB + (32*j +  2 + (j/4)*2 + 65*32)*4];
+        // Tree reduce
+        for (int j = 4; j > 0; j >>= 1)
+            for (int i = 0; i < j; i++)
+                c2[i] = _add(c2[i], c2[i+j]);
+        float * WC;
+        WC = C + m_index[ty] * N  + blockIdx.x * BLOCK_SIZE_N + tx *2;
+        if(ty < index_end-index_start)
+            *(float2*)WC = c2[0];
 
-            for (int i = 0; i < 8; i++)
-                for (int j = 0; j < 4; j++)
-                    regC[i][j] += regA[i] * regB[j];
+        __syncthreads();
+        *(float4*)&fShare[storC + 0*32*8] = *(float4*)regC[4];
+        *(float4*)&fShare[storC + 1*32*8] = *(float4*)regC[5];
+        *(float4*)&fShare[storC + 2*32*8] = *(float4*)regC[6];
+        *(float4*)&fShare[storC + 3*32*8] = *(float4*)regC[7];
+        __syncthreads();
+
+        for (int i = 0; i < 8; i++)
+            c2[i] = *(float2*)&fShare[readC + i*32];
+
+        // Tree reduce
+        for (int j = 4; j > 0; j >>= 1)
+            for (int i = 0; i < j; i++)
+                c2[i] = _add(c2[i], c2[i+j]);
+
+        WC = C + m_index[ty+16] * N  + blockIdx.x * BLOCK_SIZE_N + tx *2;
+        if(ty + 16 < index_end - index_start){
+            *(float2*)WC = c2[0];
         }
+
     }
-
-    asm volatile ("mov.u32 %0, %tid.x;"   : "=r"(tid)   :);
-    asm volatile ("mov.u32 %0, %ctaid.x;" : "=r"(bx)   :);
-    asm volatile ("mov.u32 %0, %ctaid.y;" : "=r"(by) :);
-
-    ty = ((tid & 16) >> 3) + (tid & 1);
-    tx = ((tid >> 1) & 7) + ((tid & 224) >> 2) + (ty << 2);
-
-    uint storC = ty*32*8*4 + tx*4;
-
-    tx = tid % 16;
-    ty = tid / 16;
-
-    uint readC = ty*32*8 + tx*2 + ((tid & 192)>>2);
-
-    // C should be row major
-    C += (bx * BLOCK_SIZE_M + ty) * N + (by * BLOCK_SIZE_N + tx * 2);
-
-    __syncthreads();
-    *(float4*)&fShare[storC + 0*32*8] = *(float4*)regC[0];
-    *(float4*)&fShare[storC + 1*32*8] = *(float4*)regC[1];
-    *(float4*)&fShare[storC + 2*32*8] = *(float4*)regC[2];
-    *(float4*)&fShare[storC + 3*32*8] = *(float4*)regC[3];
-    __syncthreads();
-
-    float2 c2[8];
-    for (int i = 0; i < 8; i++)
-        c2[i] = *(float2*)&fShare[readC + i*32];
-
-    // Tree reduce
-    for (int j = 4; j > 0; j >>= 1)
-        for (int i = 0; i < j; i++)
-            c2[i] = _add(c2[i], c2[i+j]);
-
-    *(float2*)C = c2[0];
-    // if(blockIdx.z == 1 && blockIdx.x==0 && blockIdx.y == 0){
-    //     printf("c2:%f \n", *(float*)(&c2[0]));
-    // }
-    __syncthreads();
-    *(float4*)&fShare[storC + 0*32*8] = *(float4*)regC[4];
-    *(float4*)&fShare[storC + 1*32*8] = *(float4*)regC[5];
-    *(float4*)&fShare[storC + 2*32*8] = *(float4*)regC[6];
-    *(float4*)&fShare[storC + 3*32*8] = *(float4*)regC[7];
-    __syncthreads();
-
-    for (int i = 0; i < 8; i++)
-        c2[i] = *(float2*)&fShare[readC + i*32];
-
-    // Tree reduce
-    for (int j = 4; j > 0; j >>= 1)
-        for (int i = 0; i < j; i++)
-            c2[i] = _add(c2[i], c2[i+j]);
-
-    C += 16 * N;
-    *(float2*)C = c2[0];
-
     
 }
 
@@ -327,30 +333,32 @@ void forward_function(
     int in_hidden,
     int out_hidden,
     int n_expert,
+    const int GLOBAL_M,
     const int TMAX
     )
 {
- 
+    const int BLOCK_SIZE_M = 32;
+    const int BLOCK_SIZE_K = 64;
+    const int BLOCK_SIZE_N = 32;
+    const int max_block = (GLOBAL_M -1 + BLOCK_SIZE_M)/BLOCK_SIZE_M;
+    dim3 blockDim(256);
+    dim3 gridDim(out_hidden/BLOCK_SIZE_N, max_block, n_expert);
+    BATCH_BLOCK_SPARSE_MATMUL<<<gridDim, blockDim>>>(tokens, sparse_index, expert_count, weight, output, GLOBAL_M, in_hidden, out_hidden, TMAX);
 }
 
 
 void moe_sparse_convert_index(
-    torch::Tensor tokens,
-    torch::Tensor weight,
     torch::Tensor router_index,
     torch::Tensor sparse_index,
     torch::Tensor expert_count
 )
 {
     // tokens: [total_token, in_hidden]
-    int total_token = tokens.size(0);
-    int in_hidden = tokens.size(1);
-    // weight : [n_expert, out_hidden, in_hidden]
-    int n_expert = weight.size(0);
-    int out_hidden = weight.size(1);
+    int total_token = router_index.size(0);
+    int n_expert = expert_count.size(0);
     const int TMAX = sparse_index.size(1); // the max token each expert can take
     assert(router_index.size(0) == total_token);
-    AT_DISPATCH_FLOATING_TYPES(tokens.type(), "seqlen_dynamic_sparse_attention", ([&]
+    AT_DISPATCH_INTEGRAL_TYPES(router_index.type(), "seqlen_dynamic_sparse_attention", ([&]
                             { convert_index(
                                     router_index.data_ptr<int>(),
                                     sparse_index.data_ptr<int>(),
@@ -369,7 +377,8 @@ at::Tensor moe_sparse_forward(
     torch::Tensor weight,
     torch::Tensor router_index,
     torch::Tensor sparse_index,
-    torch::Tensor expert_count
+    torch::Tensor expert_count,
+    const int GLOBAL_M
 )
 {
     // tokens: [total_token, in_hidden]
@@ -393,6 +402,7 @@ at::Tensor moe_sparse_forward(
                                     in_hidden,
                                     out_hidden,
                                     n_expert,
+                                    GLOBAL_M,
                                     TMAX
                                 ); }));
 
---
+++ test/test_sparse_moe.py
@@ -0,0 +1,31 @@
+import torch
+import sparta
+from sparta.opset.sparse_moe import DynamicSparseMoE
+
+
+def calculate_ref(data, exps, exp_ids, out_hidden):
+    n_exp = len(exps)
+    out = torch.zeros(data.size(0), out_hidden).to(data.device)
+    for eid in range(n_exp):
+        out[exp_ids == eid]=exps[eid](data[exp_ids == eid])
+    return out
+
+if __name__ == '__main__':
+    B = 32
+    S = 128
+    N_exp = 16
+    in_hidden = 768
+    out_hidden = 1024
+    exps = []
+    for i in range(N_exp):
+        exps.append(torch.nn.Linear(in_hidden, out_hidden, bias=False).cuda())
+    # for i in range(N_exp):
+    #     exps[i].weight.data[:]=1
+    moe = DynamicSparseMoE(N_exp, exps)
+    data = torch.rand(B*S, in_hidden).cuda()
+    exp_ids = torch.randint(0, N_exp, (B*S,)).to(torch.int32).cuda()
+    # import ipdb; ipdb.set_trace()
+    out = moe(data, exp_ids)
+    ref_out =  calculate_ref(data, exps, exp_ids, out_hidden)
+    # import ipdb; ipdb.set_trace()
+    # pass
