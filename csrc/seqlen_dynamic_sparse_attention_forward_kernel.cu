7a46ba4b1a3aafe812d65b2995fa3306cf656bc3
nizhen <nizhen>
2023-03-25 11:36:49 +0900

    update

--- csrc/seqlen_dynamic_sparse_attention_forward_kernel.cu
+++ csrc/seqlen_dynamic_sparse_attention_forward_kernel.cu
@@ -758,31 +758,31 @@ void seqlen_dynamic_forward_function(c10::Half* Q, c10::Half* K, c10::Half* V,
         const int ROWTILE = 8;
         const dim3 softBlock(32*ROWTILE);
         const dim3 softGrid(128/ROWTILE, head_num, batch_size);
-        SPARSE_SOFTMAX_FP16<128, 128, 128, ROWTILE><<<softGrid, softBlock>>>(inter_result, seqlens);
+        SPARSE_SOFTMAX_FP16<128, 128, 128, ROWTILE><<<softGrid, softBlock>>>((half*)inter_result, seqlens);
     }else if(max_seq_length==256 && hidden_dim==64){
         BLOCK_SPARSE_MATMUL_OUT_FP16<256, 64, 256, BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N, N_WARP><<<dimGrid, dimBlock>>>((half*)Q, (half*)K, (half*)inter_result, seqlens);
         const int ROWTILE = 8;
         const dim3 softBlock(32*ROWTILE);
         const dim3 softGrid(256/ROWTILE, head_num, batch_size);
-        SPARSE_SOFTMAX_FP16<256, 256, 256, ROWTILE><<<softGrid, softBlock>>>(inter_result, seqlens);
+        SPARSE_SOFTMAX_FP16<256, 256, 256, ROWTILE><<<softGrid, softBlock>>>((half*)inter_result, seqlens);
     }else if(max_seq_length==512 && hidden_dim==64){
         BLOCK_SPARSE_MATMUL_OUT_FP16<512, 64, 512, BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N, N_WARP><<<dimGrid, dimBlock>>>((half*)Q, (half*)K, (half*)inter_result, seqlens);
         const int ROWTILE = 8;
         const dim3 softBlock(32*ROWTILE);
         const dim3 softGrid(512/ROWTILE, head_num, batch_size);
-        SPARSE_SOFTMAX_FP16<512, 512, 512, ROWTILE><<<softGrid, softBlock>>>(inter_result, seqlens);
+        SPARSE_SOFTMAX_FP16<512, 512, 512, ROWTILE><<<softGrid, softBlock>>>((half*)inter_result, seqlens);
     }else if(max_seq_length==1024 && hidden_dim==64){
         BLOCK_SPARSE_MATMUL_OUT_FP16<1024, 64, 1024, BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N, N_WARP><<<dimGrid, dimBlock>>>((half*)Q, (half*)K, (half*)inter_result, seqlens);
         const int ROWTILE = 8;
         const dim3 softBlock(32*ROWTILE);
         const dim3 softGrid(1024/ROWTILE, head_num, batch_size);
-        SPARSE_SOFTMAX_FP16<1024, 1024, 1024, ROWTILE><<<softGrid, softBlock>>>(inter_result, seqlens);
+        SPARSE_SOFTMAX_FP16<1024, 1024, 1024, ROWTILE><<<softGrid, softBlock>>>((half*)inter_result, seqlens);
     }else if(max_seq_length==4096 && hidden_dim==64){
         BLOCK_SPARSE_MATMUL_OUT_FP16<4096, 64, 4096, BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N, N_WARP><<<dimGrid, dimBlock>>>((half*)Q, (half*)K, (half*)inter_result, seqlens);
         const int ROWTILE = 8;
         const dim3 softBlock(32*ROWTILE);
         const dim3 softGrid(4096/ROWTILE, head_num, batch_size);
-        SPARSE_SOFTMAX_FP16<4096, 4096, 4096, ROWTILE><<<softGrid, softBlock>>>(inter_result, seqlens);
+        SPARSE_SOFTMAX_FP16<4096, 4096, 4096, ROWTILE><<<softGrid, softBlock>>>((half*)inter_result, seqlens);
     }else{
         // please add more shape here
         assert(false);
