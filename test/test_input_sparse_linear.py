e36620378c212e93bab7d26792538ce7ea2d9dad
nizhen <nizhen>
2023-03-30 16:44:38 +0900

    update

--- test/test_input_sparse_linear.py
+++ test/test_input_sparse_linear.py
@@ -3,22 +3,22 @@ import time
 from sparta.opset.input_sparse_linear import InSparseLinear
 
 for sparsity in [0.98, 0.985, 0.99, 0.995, 0.999]:
-    linear = torch.nn.Linear(3072, 768, bias=False).cuda()
+    linear = torch.nn.Linear(20480, 5120, bias=False).cuda()
     in_linear = InSparseLinear(linear)
     # data = torch.ones(32, 128, 3072).cuda()
-    data = torch.rand(32, 128, 3072).cuda()
-    seqlens = torch.zeros(32, dtype=torch.int32, device=data.device)
-    seqlens[:]=64
+    data = torch.rand(1, 128, 20480).cuda()
+    seqlens = torch.zeros(1, dtype=torch.int32, device=data.device)
+    seqlens[:]=32
     mask = data < sparsity
     data[mask] = 0
-    # RUNTIME = 10000
-    # torch.cuda.synchronize()
-    # t_start = time.time()
-    # for _ in range(RUNTIME):
-    #     ref_out = linear(data)
-    # torch.cuda.synchronize()
-    # t_end = time.time()
-    # print('Ori Dense time: ', (t_end-t_start)*1000/RUNTIME)
+    RUNTIME = 1000
+    torch.cuda.synchronize()
+    t_start = time.time()
+    for _ in range(RUNTIME):
+        ref_out = linear(data)
+    torch.cuda.synchronize()
+    t_end = time.time()
+    print('Ori Dense time: ', (t_end-t_start)*1000/RUNTIME)
 
     # RUNTIME = 10000
     # torch.cuda.synchronize()
@@ -38,6 +38,6 @@ for sparsity in [0.98, 0.985, 0.99, 0.995, 0.999]:
         in_linear(data)
     torch.cuda.synchronize()
     t_end = time.time()
-    print(in_linear.row[4096//32]/(4096*96))
+    # print(in_linear.row[128//32]/(4096*96))
     print('Convert time: ', (t_end-t_start)*1000/RUNTIME)
 # import ipdb; ipdb.set_trace()
